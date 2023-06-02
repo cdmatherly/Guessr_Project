132ed3d14f245165d282800cbb25e114410dd2ed
Chase <chsmatherly@gmail.com>
2023-06-01 22:44:57 -0700

    some visual changes

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = (props) => {
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <Link to="/" className="flex items-center">
                     <img src={require('../static/favicon.webp')} className="h-8 mr-3 " alt="NirnGuessr Logo" />
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Nirn<span className="bg-clip-text text-transparent bg-gradient-to-l to-yellow-500 from-yellow-700">Guessr</span></span>
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Nirn<span className="bg-clip-text text-transparent bg-gradient-to-l to-yellow-400 from-yellow-600">Guessr</span></span>
                 </Link>
                 <button onClick={(e) => {setIsClosed(!isClosed)} } data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm rounded-lg md:hidden focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                 <span className="sr-only">Open main menu</span>
@@ -29,10 +29,10 @@ const Navbar = (props) => {
             </div> */}
             </div>
         </nav>
-        <div class={`w-full sm:hidden absolute duration-200 bottom-0 ` + (isClosed? ` -translate-y-48` : ` translate-y-10`)} id="navbar-solid-bg">
-            <ul class="flex flex-col items-end font-medium mt-4 rounded-lg bg-gray-50 space-x-8 border-0 bg-transparent dark:bg-gray-800 dark:bg-transparent dark:border-gray-700">
+        <div className={`w-full sm:hidden absolute duration-200 bottom-0 ` + (isClosed? ` -translate-y-48` : ` translate-y-10`)} id="navbar-solid-bg">
+            <ul className="flex flex-col items-end font-medium mt-4 rounded-lg bg-gray-50 space-x-8 border-0 bg-transparent dark:bg-gray-800 dark:bg-transparent dark:border-gray-700">
                 <li>
-                <Link to="/about" class="inline-block py-2 pl-3 pr-4 text-white rounded bg-transparent p-0 bg-gradient-to-br from-slate-800 to-slate-900 " aria-current="page">About</Link>
+                <Link to="/about" className="inline-block py-2 pl-3 pr-4 text-white rounded bg-transparent p-0 bg-gradient-to-br from-slate-800 to-slate-900 " aria-current="page">About</Link>
                 </li>
             </ul>
         </div>
--- client/src/Views/ExpansionPick.jsx
+++ client/src/Views/ExpansionPick.jsx
@@ -33,18 +33,40 @@ const ExpansionPick = (props) => {
                     {isLoading && (
                         <ThreeDots color="rgb(252, 182, 24)" ariaLabel="three-dots-loading"/>
                     )}
-
                     {!isLoading && (
                         expansions.map((expansion) => 
                         <ExpansionSelect key={expansion.expansionId} expansion={expansion} gameState={gameState} dispatch={dispatch}/>
                         )
-                        )}
+                    )}
                 </div>
-                <div className="flex justify-center relative w-full">
-                    { !hasSelection && (
-                        <span className="absolute text-red-400 -top-8">Please select an expansion</span>
-                    ) }
-                    <button onClick={() => dispatch({type: "startPlaying"}) } type="button" className={`text-gray-900 bg-gradient-to-r from-yellow-500 to-yellow-600 hover:bg-gradient-to-b focus:ring-4 focus:outline-none focus:ring-yellow-400 font-medium rounded-3xl text-md px-5 py-2.5 text-center disabled:from-slate-500 disabled:to-slate-600 disabled:bg-gradient-to-r` } style={{transition:'300ms ease'}} disabled={hasSelection ? false : true }>Play</button>
+                <div className="flex justify-center w-1/3 relative">
+                    {/* <div className={`flex flex-col gap-3 justify-center absolute top-16 z-20 bg-slate-600 bg-opacity-70 px-2 py-3 rounded-md text-center ` + (gameState.includeBaseGame? ` duration-300 translate-y-0` : ` -translate-y-5 blur-sm opacity-0`)}>
+                    {gameState.includeBaseGame && (
+                    <>
+                        <label class="relative inline-flex items-center mr-5 cursor-pointer">
+                            <input onChange={(e) => {} } type="checkbox" value="" class="sr-only peer"/>
+                            <div class="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-yellow-400 dark:peer-focus:ring-yellow-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-yellow-500"></div>
+                            <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Imperial City</span>
+                        </label>
+                        <label class="relative inline-flex items-center mr-5 cursor-pointer">
+                            <input onChange={(e) => {} } type="checkbox" value="" class="sr-only peer"/>
+                            <div class="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-yellow-400 dark:peer-focus:ring-yellow-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-yellow-500"></div>
+                            <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Thieves' Guild</span>
+                        </label>
+                        <label class="relative inline-flex items-center mr-5 cursor-pointer">
+                            <input onChange={(e) => {} } type="checkbox" value="" class="sr-only peer"/>
+                            <div class="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-yellow-400 dark:peer-focus:ring-yellow-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-yellow-500"></div>
+                            <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Dark Brotherhood</span>
+                        </label>
+                    </>
+                    )}
+                    </div> */}
+                    <div className="flex justify-center relative w-full">
+                        { !hasSelection && (
+                            <span className="absolute text-red-400 -top-8">Please select an expansion</span>
+                            ) }
+                        <button onClick={() => dispatch({type: "startPlaying"}) } type="button" className={`text-gray-900 bg-gradient-to-r from-yellow-500 to-yellow-600 hover:bg-gradient-to-b focus:ring-4 focus:outline-none focus:ring-yellow-400 font-medium rounded-3xl text-md px-5 py-2.5 text-center disabled:from-slate-500 disabled:to-slate-600 disabled:bg-gradient-to-r ` } style={{transition:'500ms ease'}} disabled={hasSelection ? false : true }>Play</button>
+                    </div>
                 </div>
             </div>
         </>
