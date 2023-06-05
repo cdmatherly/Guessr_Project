67977b54c0e54ba1e31d8fb0f96f45b5105e55ce
Chase <chsmatherly@gmail.com>
2023-06-05 15:46:14 -0700

    added media query library

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import MediaQueryReact from 'media-query-react';
 import Index from './Views/Index';
 import Viewer from './Views/Viewer';
 import Home from './Views/Home';
@@ -8,15 +9,37 @@ import Info from './Views/Info';
 import NotFound from './Views/NotFound';
 
 function App() {
+
+  const screenSize = {
+    mobile: { 
+      minWidth: 320,
+      maxWidth: 480,
+    },
+    mobileLandscape: {
+      minWidth: 481,
+      maxWidth: 767,
+    },
+    tablet: {
+      minWidth: 768,
+      maxWidth: 1024,
+    },
+    desktop: {
+      minWidth: 1025,
+      maxWidth: 2500,
+    },
+  }
+
   return (
-    <Routes>
-      <Route element={<Index />}>
-        <Route element={<Home />} path='/' />
-        <Route element={<Player />} path='/play'/>
-        <Route element={<Info />} path='/about' />
-        <Route element={<NotFound/>} path='/*'/>
-      </Route>
-    </Routes>
+    <MediaQueryReact mediaQueries={screenSize}>
+      <Routes>
+        <Route element={<Index />}>
+          <Route element={<Home />} path='/' />
+          <Route element={<Player />} path='/play'/>
+          <Route element={<Info />} path='/about' />
+          <Route element={<NotFound/>} path='/*'/>
+        </Route>
+      </Routes>
+    </MediaQueryReact>
   );
 }
 
